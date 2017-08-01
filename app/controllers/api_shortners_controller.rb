75d1110c0ae1846c66501eb66af0016bbd24ee20
mohan <mohan589@gmail.com>
2017-08-01 22:33:34 +0530

    changes for slug

--- app/controllers/api_shortners_controller.rb
+++ app/controllers/api_shortners_controller.rb
@@ -1,5 +1,6 @@
 class ApiShortnersController < ApplicationController
   include ActionController::MimeResponds
+  before_action :set_url, only: [:show]
 
   respond_to :html, :js
 
@@ -12,8 +13,16 @@ class ApiShortnersController < ApplicationController
     @api_shortner = ApiShortner.create(shortner_params)
   end
 
+  def show
+    redirect_to @api_shortner.first.input_url
+  end
+
   private
   def shortner_params
     params.require(:api_shortner).permit(:input_url, :slug)
+  end
+
+  def set_url
+    @api_shortner = ApiShortner.where(slug: params[:id])
   end
 end
