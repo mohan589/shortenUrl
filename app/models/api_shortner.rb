02c38e55adc0608fffae87177ad0b58da74c40cb
mohan <mohan589@gmail.com>
2017-08-02 11:07:50 +0530

    changes for shortner validation

--- app/models/api_shortner.rb
+++ app/models/api_shortner.rb
@@ -1,6 +1,7 @@
 class ApiShortner < ApplicationRecord
   after_create :generate_slug
 
+  validates :input_url, :presence => true
   def generate_slug
     self.slug = self.id.to_s(36)
     self.save
